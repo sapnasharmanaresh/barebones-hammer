084cce1287ced9e1c8eec7182e94041b4f6f1f18
Ka-Chun Chau <kachun@kachunchau.com>
2013-06-12 14:17:26 +0100

    moved all the features into one page

---
+++ javascript/main.js
@@ -0,0 +1,10 @@
+$(document).keyup(function(e) { 
+  if (e.keyCode == 27) { // esc keycode
+    $('.overlay').removeClass('is-shown');
+  }
+});
+
+$('.reveal').click(function() {
+  $('.overlay').toggleClass('is-shown');
+});
+
