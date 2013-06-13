a3b05880bfbdcf7e86d9bd06fb719b8ad0a7ebbe
Ka-Chun Chau <kachun@kachunchau.com>
2013-06-13 11:29:39 +0100

    pure css modal instead of js

--- javascript/main.js
+++ javascript/main.js
@@ -1,10 +1,9 @@
-$(document).keyup(function(e) { 
-  if (e.keyCode == 27) { // esc keycode
-    $('.overlay').removeClass('is-shown');
-  }
-});
+// $(document).keyup(function(e) { 
+//   if (e.keyCode == 27) { // esc keycode
+//     $('.overlay').removeClass('is-shown');
+//   }
+// });
 
-$('.reveal').click(function() {
-  $('.overlay').toggleClass('is-shown');
-});
-
+// $('.reveal').click(function() {
+//   $('.overlay').toggleClass('is-shown');
+// });
