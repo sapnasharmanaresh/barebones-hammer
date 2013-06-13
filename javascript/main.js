f58b5bca127046f64d303b30ef3eac0496d73849
Ka-Chun Chau <kachun@kachunchau.com>
2013-06-13 12:21:58 +0100

    changed body font to sans-serif

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,8 @@
-// $(document).keyup(function(e) { 
-//   if (e.keyCode == 27) { // esc keycode
-//     $('.overlay').removeClass('is-shown');
-//   }
-// });
+$(document).keyup(function(e) { 
+  if (e.keyCode == 27) { // esc keycode
+    // $('.overlay').removeClass('is-shown');
+  }
+});
 
 // $('.reveal').click(function() {
 //   $('.overlay').toggleClass('is-shown');
