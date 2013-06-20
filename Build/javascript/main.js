a9feb07caf531e8f0a8d8f265c3822dd83739a6d
Ka-Chun Chau <kachun@kachunchau.com>
2013-06-20 11:14:16 +0100

    removed build folder

--- Build/javascript/main.js
+++
@@ -1,9 +0,0 @@
-$(document).keyup(function(e) { 
-  if (e.keyCode == 27) { // esc keycode
-    // $('.overlay').removeClass('is-shown');
-  }
-});
-
-// $('.reveal').click(function() {
-//   $('.overlay').toggleClass('is-shown');
-// });
