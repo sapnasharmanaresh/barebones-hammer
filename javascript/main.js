869e5d30003a97a6a9e21e95d8808cbfbf600636
Ka-Chun Chau <kachun@kachunchau.com>
2013-06-25 16:58:13 +0100

    fixed the responsive table for displaying header

--- javascript/main.js
+++ javascript/main.js
@@ -1,19 +1,5 @@
-$(document).keyup(function(e) { 
-  if (e.keyCode == 27) { // esc keycode
-    // $('.overlay').removeClass('is-shown');
-  }
-});
+
 
 $('.menu-link').on('click', function () {
   $('.drawer').toggleClass('is-visibile');
-});
-
-$('.reveal').click(function() {
-  // Rename is-modal
-  $('body').addClass('test');
-});
-
-$('.modal-close').click(function() {
-  // Rename is-modal
-  $('body').removeClass('test');
 });
